e3e5efb1bed6a9217f005cbf0ed076d0f9557e2d
Akash Prajapati <akashprajapati90999@gmail.com>
2025-11-03 17:14:35 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -4,7 +4,7 @@ dotenv.config();
 const JWT_USER_PASSWORD = process.env.JWT_USER_PASSWORD;
 const JWT_ADMIN_PASSWORD = process.env.JWT_ADMIN_PASSWORD;
 const STRIPE_SECRET_KEY =
-  "sk_test_51RxRcT0AGPM5iP7QyYuSqWvavLHhUAxwHt7HnNceLPvcuYril6Sdg0XRMM255SD2hoKNgidlQ25EJVVtohaSqpgj00zbvACIRB";
+  "add urs";
 
 const JWT_PROVIDER_PASSWORD = process.env.JWT_PROVIDER_PASSWORD;
 export default {
